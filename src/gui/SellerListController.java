acc0e76e1ee7319064b4aaf8db59424e08ca33f1
Cristiano Soares <cristianosoaresti@hotmail.com>
2021-02-11 13:56:52 -0300

    SellerList

---
+++ src/gui/SellerListController.java
@@ -0,0 +1,177 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+
+public class SellerListController implements Initializable, DataChangeListener {
+
+	private SellerService service;
+
+	private ObservableList<Seller> obsSeller;
+
+	@FXML
+	private Button btNew;
+
+	@FXML
+	private TableView<Seller> tableViewSeller;
+
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId;
+
+	@FXML
+	private TableColumn<Seller, String> tableColumnName;
+
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnEDIT;
+
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnREMOVE;
+
+	@FXML
+	public void onBtNewAction(ActionEvent actionEvent) {
+		Stage parentStage = Utils.currentStage(actionEvent);
+		Seller seller = new Seller();
+		createDialogForm(seller, "/gui/SellerForm.fxml", parentStage);
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNode();
+	}
+
+	private void initializeNode() {
+
+		// Initializing the column on the window as default
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+		// To resize the table view as the window
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
+	}
+
+	// With this method the developer can make dependency injection
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service is null");
+		}
+		List<Seller> listDept = service.findAll();
+		obsSeller = FXCollections.observableArrayList(listDept);
+		tableViewSeller.setItems(obsSeller);
+
+		initEditButtons();
+		initRemoveButtons();
+	}
+
+	private void createDialogForm(Seller seller, String absoluteName, Stage parentStage) {
+//		try {
+//
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+//			Pane pane = loader.load();
+//
+//			SellerFormController controller = loader.getController();
+//			controller.setSeller(seller);
+//			controller.setSellerService(new SellerService());
+//			controller.subscribeDataChangeListener(this);
+//			controller.updateFormData();
+//
+//			Stage dialogStage = new Stage();
+//			dialogStage.setTitle("Enter with seller data");
+//			dialogStage.setScene(new Scene(pane));
+//			dialogStage.setResizable(false);
+//			dialogStage.initOwner(parentStage);
+//			dialogStage.initModality(Modality.WINDOW_MODAL);
+//			dialogStage.showAndWait();
+//
+//		} catch (IOException e) {
+//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+//		}
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("Edit");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("Remove");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+	private void removeEntity(Seller seller) {
+		Optional<ButtonType> result =  Alerts.showConfirmation("Confirmation", "Are you sure that you want to delete it?");
+		
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Service is null");
+			}
+			try {
+				service.remove(seller);
+				updateTableView();
+			} catch (DbException e) {
+				Alerts.showAlert("Error removing seller", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+	}
+
+	// This class implements DataChangeListener
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+	}
+}
